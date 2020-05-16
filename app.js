be16bd0c39df46ea2bb7765e87de8f5fa819adcf
Edgar <edgar.lilienthal@khk.ee>
2020-05-16 12:47:18 +0300

    Backend working over https with letsencrypt [#172872292]

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ db.once('open', function () {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
